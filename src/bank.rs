789a97bc471c237d9b71c9cab759dace27f57aea
= <pmosadeqzadeh@gmail.com>
2025-07-09 18:27:41 +0330

    fix: made dep and draw shorter

--- src/bank.rs
+++ src/bank.rs
@@ -12,7 +12,7 @@ impl Bank {
     pub fn deposit(&mut self, account_id: u32, balance: i32) {
         for ele in &mut self.accounts {
             if ele.id == account_id {
-                ele.balance = ele.balance + balance;
+                ele.balance += balance;
             }
         }
     }
@@ -20,7 +20,7 @@ impl Bank {
     pub fn withdraw(&mut self, account_id: u32, amount: i32) {
         for ele in &mut self.accounts {
             if ele.id == account_id {
-                ele.balance = ele.balance - amount;
+                ele.balance -= amount;
             }
         }
     }
